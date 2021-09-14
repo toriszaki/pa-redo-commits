7119ee0673d092ff00a9d92d01ba8c762d595dd0
unknown <toriszaki@gmail.com>
2021-09-14 13:18:26 +0200

    get_numbers repeated code deleted

--- pax.py
+++ pax.py
@@ -11,17 +11,16 @@ def get_numbers(points, point_name):
 
 def get_random_numbers(points, point_name):
     points.append((random.random() * 10 - 5, random.random() * 10 - 5))
-    print("P3x is " + str(points[2][0]))
-    print("P3y is " + str(points[2][1]))
+    print(f"{point_name}x is " + str(points[2][0]))
+    print(f"{point_name}y is " + str(points[2][1]))
 
 
-
-def calculate_distance():
+def calculate_distance(points):
     x = 0
-    a = pts[0]
-    b = pts[1]
-    for p in pts[::-1]:
-        for q in pts[::-1]:
+    a = points[0]
+    b = points[1]
+    for p in points[::-1]:
+        for q in points[::-1]:
             if math.sqrt((p[0] - q[0]) * (p[0] - q[0]) + math.pow(p[1] - q[1], 2)) > x:
                 x = math.sqrt(math.pow(p[1] - q[1], 2) + (p[0] - q[0]) * (p[0] - q[0]))
                 a = p
@@ -32,7 +31,10 @@ def calculate_distance():
 def main():
     print("Welcome to my mystery program!")
     points = []
-    get_numbers(points, point_name)
-    get_random_numbers(points, point_name)
+    get_numbers(points, "A")
+    get_numbers(points, "B")
+    get_random_numbers(points, "C")
     a, b, x = calculate_distance(points)
     print("A " + str(a) + ", B" + str(b) + ", d:" + str(x) )
+
+main()
