103ce9f4307a77411a34a747c5fb7a3b04ba1cfb
unknown <toriszaki@gmail.com>
2021-09-14 11:37:06 +0200

    creating calculate_distance

--- pax.py
+++ pax.py
@@ -2,31 +2,32 @@ import random
 import math
 
 def get_number():
-a=float(input("P1x: "))
-print("P1x is " + str(a))
-b=float(input("P1y: "))
-print("P1y is " + str(b))
-c=float(input("P2x: "))
-print("P2x is " + str(c))
-d=float(input("P2y: "))
-print("P2y is " + str(d))
-pts = []
+    a=float(input("P1x: "))
+    print("P1x is " + str(a))
+    b=float(input("P1y: "))
+    print("P1y is " + str(b))
+    c=float(input("P2x: "))
+    print("P2x is " + str(c))
+    d=float(input("P2y: "))
+    print("P2y is " + str(d))
+    pts = []
+    pts.append((a,b))
+    pts.append((c,d))
+    pts.append((random.random() * 10 - 5, random.random() * 10 - 5))
+    print("P3x is " + str(pts[2][0]))
+    print("P3y is " + str(pts[2][1]))
 
-pts.append((a,b))
-pts.append((c,d))
-pts.append((random.random() * 10 - 5, random.random() * 10 - 5))
-print("P3x is " + str(pts[2][0]))
-print("P3y is " + str(pts[2][1]))
 
-x = 0
-a = pts[0]
-b = pts[1]
-for p in pts[::-1]:
-    for q in pts[::-1]:
-        if math.sqrt((p[0] - q[0]) * (p[0] - q[0]) + math.pow(p[1] - q[1], 2)) > x:
-            x = math.sqrt(math.pow(p[1] - q[1], 2) + (p[0] - q[0]) * (p[0] - q[0]))
-            a = p
-            b = q
+def calculate_distance():
+    x = 0
+    a = pts[0]
+    b = pts[1]
+    for p in pts[::-1]:
+        for q in pts[::-1]:
+            if math.sqrt((p[0] - q[0]) * (p[0] - q[0]) + math.pow(p[1] - q[1], 2)) > x:
+                x = math.sqrt(math.pow(p[1] - q[1], 2) + (p[0] - q[0]) * (p[0] - q[0]))
+                a = p
+                b = q
 
 
 def main():
