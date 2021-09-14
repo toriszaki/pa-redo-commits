9fe364df87b38889757343aefd137fdefc5d6c09
unknown <toriszaki@gmail.com>
2021-09-14 14:26:43 +0200

    calculating_distance variable

--- pax.py
+++ pax.py
@@ -19,11 +19,12 @@ def get_random_numbers(points, point_name):
 
 def calculate_distance(points):
     distance = 0
-    for p in points[::-1]:
+    for first_coor_to_compare in points[::-1]:
         for q in points[::-1]:
-            if math.sqrt((p[0] - q[0]) * (p[0] - q[0]) + math.pow(p[1] - q[1], 2)) > distance:
-                distance = math.sqrt(math.pow(p[1] - q[1], 2) + (p[0] - q[0]) * (p[0] - q[0]))
-                a = p
+            calculating_distance = math.sqrt(math.pow(first_coor_to_compare[1] - q[1], 2) + (first_coor_to_compare[0] - q[0]) * (first_coor_to_compare[0] - q[0]))
+            if calculating_distance > distance:
+                distance = math.sqrt(math.pow(first_coor_to_compare[1] - q[1], 2) + (first_coor_to_compare[0] - q[0]) * (first_coor_to_compare[0] - q[0]))
+                a = first_coor_to_compare
                 b = q
     return a, b, distance
 
